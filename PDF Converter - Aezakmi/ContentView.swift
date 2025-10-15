eb83c8398c38b849d9b3c8bd6bbf893a4168dd25
Andrey Kaldyaev <out_KaldyaevAV@mkb.ru>
2025-10-15 18:10:03 +0300

    Initial Commit

---
+++ PDF Converter - Aezakmi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDF Converter - Aezakmi
+//
+//  Created by Andrey Kaldyaev on 15.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
